476abb2a2034d0bc7158b2935ac56a0062a82a19
Paul Sodimu <paul.sodimu@hotmail.co.uk>
2018-03-13 17:56:24 -0500

    Add project files.

---
+++ Bayological.DnaLogger/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bayological.DnaLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
